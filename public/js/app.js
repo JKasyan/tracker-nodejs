e6e30f6c25c12a70e6daf2ebb4953eea8028ad90
JKasyan <j.kasyan86@gmial.com>
2016-11-29 23:53:50 +0200

    angular

--- public/js/app.js
+++ public/js/app.js
@@ -1,32 +1,41 @@
 /**
  * Created by kasyan on 11/25/16.
  */
-angular.module('trackerApp', ['services'])
-    .controller('pointsController', ['$log', '$scope', 'pointsService',function ($log, $scope, pointsService) {
-        $log.debug(Object.keys($scope));
-        pointsService.lastPoint().then(function (response) {
-            $log.debug('response data =', response.data);
-            $scope.point = response.data[0];
-        });
-        var urls = ['https://pp.vk.me/c837434/v837434471/df38/Ai31C8HOtgU.jpg',
-            'https://pp.vk.me/c543107/v543107888/3a566/pFXO6YYaTkA.jpg',
-        'https://pp.vk.me/c836727/v836727420/ec8b/h7m7sAoxrp8.jpg'];
-        var i = 0;
-        $scope.url = urls[i];
-        $scope.changeUrl = function() {
-            $scope.url = urls[++i % urls.length];
+var app = angular.module('trackerApp', ['services'])
+
+    .controller('trackByDateController', ['$log', '$scope', 'mapService', function ($log, $scope, mapService) {
+        $log.debug('trackByDateController');
+        $scope.allDigits = mapService.all();
+        //
+        $scope.remove = function(index) {
+            $log.debug(index);
+            mapService.remove(index);
         }
-        $scope.date = new Date();
-        //$interval($scope.changeUrl, 5000);
-        $scope.car = 'BMW';
-        $scope.cars = [];
-        $scope.addCar = function() {
-            $scope.cars.push($scope.car);
+    }])
+
+    .controller('mapController', mapController);
+
+function mapController($scope, $log, pointsService, mapService) {
+    $log.debug('mapController');
+    $scope.addDigit = function(digit) {
+        mapService.add(angular.copy(digit));
+    }
+    $scope.data = mapService.all();
+}
+
+mapController.$inject = ['$scope', '$log', 'pointsService', 'mapService'];
+
+app.factory('mapService', function() {
+    var data = [1,2,3];
+    return {
+        add:function(digit) {
+            data.push(digit);
+        },
+        all: function() {
+            return data;
+        },
+        remove: function(index) {
+            data.splice(index, 1);
         }
-        $scope.remove = function() {
-            $scope.cars.pop();
-        }
-        $scope.$watch('cars', function(newCar, oldCar) {
-            $log.debug($scope.cars.length, ' ', newCar, ' ', oldCar);
-        }, true);
-    }]);
+    }
+});
